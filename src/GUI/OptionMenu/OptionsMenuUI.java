445511a581ebbcff1c355fe176b4bffbf483bc2d
Joe McDonnell <joemcdonnell50@gmail.com>
2017-10-22 21:39:15 +0100

    Added OptionMenu and RegisterMenu code

---
+++ src/GUI/OptionMenu/OptionsMenuUI.java
@@ -0,0 +1,156 @@
+package GUI.OptionMenu;
+
+
+import GUI.ButtonHandler;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author New User
+ */
+public class OptionsMenuUI extends javax.swing.JFrame {
+    
+    public static JFrame frame; 
+    
+    public static JFrame getFrame() {
+        return frame;
+    }   
+
+    //public static OptionsMenuUI menuUI; 
+    
+    //public static OptionsMenuUI getFrame(){
+    //    return menuUI; 
+    //}
+    /**
+     * Creates new form OptionsMenu
+     */
+    public OptionsMenuUI() {
+        initComponents();
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">                          
+    private void initComponents() {
+
+        jDesktopPane1 = new javax.swing.JDesktopPane();
+        jLayeredPane1 = new javax.swing.JLayeredPane();
+        ViewResButton = new javax.swing.JButton();
+        MakeResButton = new javax.swing.JButton();
+        ManageResButton = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
+        logoutButton = new javax.swing.JButton();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        
+        //Buttons
+        ViewResButton.setFont(new java.awt.Font("Dialog", 0, 18)); // NOI18N
+        ViewResButton.setText("View My Reservations");
+        ViewResButton = new OptionsMenuViewResButton();
+
+        MakeResButton.setFont(new java.awt.Font("Dialog", 0, 18)); // NOI18N
+        MakeResButton.setText("Make Reservation");
+        MakeResButton = new OptionsMenuMakeResButton();
+
+        ManageResButton.setFont(new java.awt.Font("Dialog", 0, 18)); // NOI18N
+        ManageResButton.setText("Manage Reservations");
+        ManageResButton = new OptionsMenuManageResButton();
+
+        logoutButton.setText("Logout");
+        logoutButton = new OptionsMenuLogoutButton();
+        
+        //Labels
+        jLabel1.setFont(new java.awt.Font("Dialog", 2, 18)); // NOI18N
+        jLabel1.setText("                    Please select an option: ");
+        
+        //ButtonHandler for Command DP
+        ButtonHandler vf = new ButtonHandler();  
+        ManageResButton.addActionListener(vf);
+        MakeResButton.addActionListener(vf);
+        ViewResButton.addActionListener(vf);
+        logoutButton.addActionListener(vf); 
+
+        jLayeredPane1.setLayer(ViewResButton, javax.swing.JLayeredPane.DEFAULT_LAYER);
+        jLayeredPane1.setLayer(MakeResButton, javax.swing.JLayeredPane.DEFAULT_LAYER);
+        jLayeredPane1.setLayer(ManageResButton, javax.swing.JLayeredPane.DEFAULT_LAYER);
+        jLayeredPane1.setLayer(jLabel1, javax.swing.JLayeredPane.DEFAULT_LAYER);
+        jLayeredPane1.setLayer(logoutButton, javax.swing.JLayeredPane.DEFAULT_LAYER);
+
+        javax.swing.GroupLayout jLayeredPane1Layout = new javax.swing.GroupLayout(jLayeredPane1);
+        jLayeredPane1.setLayout(jLayeredPane1Layout);
+        jLayeredPane1Layout.setHorizontalGroup(
+            jLayeredPane1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jLayeredPane1Layout.createSequentialGroup()
+                .addGroup(jLayeredPane1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jLayeredPane1Layout.createSequentialGroup()
+                        .addGap(200, 200, 200)
+                        .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 383, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(jLayeredPane1Layout.createSequentialGroup()
+                        .addGap(304, 304, 304)
+                        .addGroup(jLayeredPane1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(MakeResButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(ViewResButton, javax.swing.GroupLayout.DEFAULT_SIZE, 202, Short.MAX_VALUE)
+                            .addComponent(ManageResButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
+                .addContainerGap(202, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jLayeredPane1Layout.createSequentialGroup()
+                .addGap(0, 0, Short.MAX_VALUE)
+                .addComponent(logoutButton)
+                .addGap(24, 24, 24))
+        );
+        jLayeredPane1Layout.setVerticalGroup(
+            jLayeredPane1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jLayeredPane1Layout.createSequentialGroup()
+                .addGap(23, 23, 23)
+                .addComponent(logoutButton)
+                .addGap(45, 45, 45)
+                .addComponent(jLabel1)
+                .addGap(57, 57, 57)
+                .addComponent(MakeResButton, javax.swing.GroupLayout.PREFERRED_SIZE, 61, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(50, 50, 50)
+                .addComponent(ViewResButton, javax.swing.GroupLayout.PREFERRED_SIZE, 61, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(50, 50, 50)
+                .addComponent(ManageResButton, javax.swing.GroupLayout.PREFERRED_SIZE, 61, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(120, Short.MAX_VALUE))
+        );
+
+        jDesktopPane1.setLayer(jLayeredPane1, javax.swing.JLayeredPane.DEFAULT_LAYER);
+
+        javax.swing.GroupLayout jDesktopPane1Layout = new javax.swing.GroupLayout(jDesktopPane1);
+        jDesktopPane1.setLayout(jDesktopPane1Layout);
+        jDesktopPane1Layout.setHorizontalGroup(
+            jDesktopPane1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jLayeredPane1)
+        );
+        jDesktopPane1Layout.setVerticalGroup(
+            jDesktopPane1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jLayeredPane1)
+        );
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 785, Short.MAX_VALUE)
+            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addComponent(jDesktopPane1))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 577, Short.MAX_VALUE)
+            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addComponent(jDesktopPane1))
+        );
+
+        pack();
+    }// </editor-fold>                        
+
+    // Variables declaration - do not modify                     
+    private javax.swing.JButton MakeResButton;
+    private javax.swing.JButton ManageResButton;
+    private javax.swing.JButton ViewResButton;
+    private javax.swing.JButton logoutButton;
+    private javax.swing.JDesktopPane jDesktopPane1;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLayeredPane jLayeredPane1;
+    // End of variables declaration
+    
+}
---
+++ src/GUI/RegisterMenu/OffersCheckBox.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GUI.RegisterMenu;
+
+import GUI.Command;
+import javax.swing.JCheckBox;
+
+/**
+ *
+ * @author New User
+ */
+public class OffersCheckBox extends JCheckBox implements Command {
+
+    @Override
+    public void execute() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
