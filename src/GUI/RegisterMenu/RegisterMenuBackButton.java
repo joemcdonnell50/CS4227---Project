4efa00de938beca079044bb03e944c1bb472d42f
Joe McDonnell <joemcdonnell50@gmail.com>
2017-10-27 15:47:18 +0100

    Updated makeVisible call

--- src/GUI/RegisterMenu/RegisterMenuBackButton.java
+++ src/GUI/RegisterMenu/RegisterMenuBackButton.java
@@ -16,7 +16,6 @@ public class RegisterMenuBackButton extends JButton implements Command {
 
     @Override
     public void execute() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+        OptionsMenuUI.makeVisible();
     
 }
