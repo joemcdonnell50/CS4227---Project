3efb64a9c63816b826115c19bc9d7dc4299e5f6a
JackCronin <14142031@studentmail.ul.ie>
2017-11-01 23:39:52 +0000

    upload payment

---
+++ src/HotelSystem/Entities/Payment.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package HotelSystem.Entities;
+
+/**
+ *
+ * @author Owner
+ */
+public class Payment extends Entity {
+
+    private int reservation_id;
+    private String user_name;
+    private String room_type;
+    private int number_of_guests;
+    private String arrival_date;
+    private String checkout_date;
+    
+    public int getReservation_id() {
+        return reservation_id;
+    }
+
+    public void setReservation_id(int reservation_id) {
+        this.reservation_id = reservation_id;
+    }
+
+    public String getUser_name() {
+        return user_name;
+    }
+
+    public void setUser_name(String user_name) {
+        this.user_name = user_name;
+    }
+
+    public String getRoom_type() {
+        return room_type;
+    }
+
+    public void setRoom_type(String room_type) {
+        this.room_type = room_type;
+    }
+
+    public int getNumber_of_guests() {
+        return number_of_guests;
+    }
+
+    public void setNumber_of_guests(int number_of_guests) {
+        this.number_of_guests = number_of_guests;
+    }
+
+    public String getArrival_date() {
+        return arrival_date;
+    }
+
+    public void setArrival_date(String arrival_date) {
+        this.arrival_date = arrival_date;
+    }
+
+    public String getCheckout_date() {
+        return checkout_date;
+    }
+
+    public void setCheckout_date(String checkout_date) {
+        this.checkout_date = checkout_date;
+    }
+    
+    
+}
