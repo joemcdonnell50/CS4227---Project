20cf685c08c06ea93f7d4998ec3a77f15446bb38
JackCronin <14142031@studentmail.ul.ie>
2017-11-05 21:41:20 +0000

    Update ReceiptToDiscountReceiptAdapter.java

--- src/HotelSystem/Receipt/ReceiptToDiscountReceiptAdapter.java
+++ src/HotelSystem/Receipt/ReceiptToDiscountReceiptAdapter.java
@@ -20,14 +20,13 @@ public class ReceiptToDiscountReceiptAdapter extends DiscountedReceipt implement
 
     @Override
     public void formatReceipt(UserReservationDetails Details) {
-          String Receipt = String.format("Hello " + Details.getUser_id() + "\n"
+         String Receipt = String.format("Hello " + Details.getUser_id() + "\n"
                 + "Receipt for reservation number " + Details.getReservation_id() + "\n"
-                + "Hotel : " + "\t" + Details.getHotelName()+ "\n"
+                + "Hotel : " + "\t" + Details.getHotel() + "\n"
                 + "Roomtype : " + "\t" + Details.getRoomType() + "\n"
-                + "Number of Guests : " + "\t" + Details.getNumberOfGuests() + "\n"
+                + "Number of Guests : " + "\t" + Details.getNumberofGuests() + "\n"
                 + "Date of Arrival : " + "\t" + Details.getArrival_date() + "\n"
                 + "Check out Date : " + "\t" + Details.getCheckout_date() + "\n"
-                + "Cost of Services :" + "\t" + Details.getServicesPrice() + "\n"
-                + "Your total cost is " + "\t" + Details.getPrice() +
-                  AddToReceipt());   }
+                + "Cost of Services :" + "\t" + Details.getServicePrice() + "\n"
+                + "Your total cost is " + "\t" + Details.getPrice()+ AddToReceipt());   }
     }
