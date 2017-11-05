4106662c50a6d2a16726dc8dfb6f65f7aa079ade
JackCronin <14142031@studentmail.ul.ie>
2017-11-05 21:39:30 +0000

    Updated Discounted Receipt

--- src/HotelSystem/Receipt/DiscountedReceipt.java
+++ src/HotelSystem/Receipt/DiscountedReceipt.java
@@ -5,13 +5,20 @@
  */
 package HotelSystem.Receipt;
 
+import HotelSystem.Entities.User;
+
 /**
  *
  * @author Owner
  */
 public class DiscountedReceipt {
 public String AddToReceipt(){
-    String Addedtext = "";
+    double Discount = 0;
+    double OrignalCost =0; 
+    User user = new User();
+    //Discount =user.getDiscount();
+    Discount = 10;
+    String Addedtext = "Due to you being a loyal costomer you have recieved a discount of : " + Discount + "\n";
     return Addedtext;
 }
 }
