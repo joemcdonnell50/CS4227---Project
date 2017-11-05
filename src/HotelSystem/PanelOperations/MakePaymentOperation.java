4df2c42848cc3e6b9fe64ee77c928ecd362266bf
JackCronin <14142031@studentmail.ul.ie>
2017-11-05 21:42:29 +0000

    Updated MakePaymentOperation

--- src/HotelSystem/PanelOperations/MakePaymentOperation.java
+++ src/HotelSystem/PanelOperations/MakePaymentOperation.java
@@ -5,7 +5,13 @@
  */
 package HotelSystem.PanelOperations;
 
+import HotelSystem.Entities.Payment;
 import HotelSystem.Entities.Reservation;
+import HotelSystem.Entities.User;
+import HotelSystem.Payment.Context;
+import HotelSystem.Payment.Discount10;
+import HotelSystem.Payment.Discount20;
+import HotelSystem.Payment.Discount30;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.format.ResolverStyle;
@@ -19,7 +25,7 @@ public class MakePaymentOperation {
     private double total;
     
     public static void makePaymentOperation() throws Exception {
-        //Payment payment = new Payment();
+        Payment payment = new Payment();
        // payment.setConfirmPaid(paymentUI.getConfirmPaid());
        
     }
@@ -45,7 +51,6 @@ public class MakePaymentOperation {
         long NumberofNight =  DAYS.between(Adate, Cdate);
         double servicePrice = 0;
         int NumberOfGuests = Reservation.getReservationInstance().getNumber_of_guests();
-        System.out.println("Ducks" + Adate);
         for (int k = 0; k <= NumberofNight; k++) {
             String day = Adate.getDayOfWeek().toString();
             if (day.equalsIgnoreCase("Saturday") || day.equalsIgnoreCase("Sunday")) {
@@ -55,8 +60,27 @@ public class MakePaymentOperation {
         }
         double WeekendCost = ((PricePerNight / 100.0) * 25);
         double Cost = ((NumberofNight * PricePerNight) * NumberOfGuests) + (WeekendCost * daysOnWeekend) + servicePrice;
+        int LoyaltyLevel = 1;
+        User user = new User();
+        //LoyaltyLevel =user.getLoyalty_level();
+        switch (LoyaltyLevel) {
+            case 1:
+                Context context = new Context(new Discount10());
+                Cost = context.executeStrategy(Cost);
+                break;
+            case 2:
+                Context contextB = new Context(new Discount20());
+                Cost =contextB.executeStrategy(Cost);
+                break;
+            case 3:
+                Context contextC = new Context(new Discount30());
+                Cost =contextC.executeStrategy(Cost);
+                break;
+            default:
+                break;
+        }
         setTotal(Cost);
-       System.out.println("Ducks" + Adate +" "+ Cdate+ " " +NumberofNight+ " " +Cost);
+       System.out.println(Adate +" "+ Cdate+ " " +NumberofNight+ " " +Cost);
 
     }
     public void addToBill(double addedvalue) {
