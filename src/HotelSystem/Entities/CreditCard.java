1f3c98a2feba1b1e7e145ebfde919341494e6ec6
jd631x <13147013@studentmail.ul.ie>
2017-11-13 02:58:29 +0000

    Adding credit card entity

---
+++ src/HotelSystem/Entities/CreditCard.java
@@ -0,0 +1,65 @@
+package HotelSystem.Entities;
+ 
+ 
+ /**
+  *
+  * @author Owner
+  */
+ public class CreditCard implements Entity{
+ 
+     private String NameOnCard;
+     private String creditCardNum;
+     private String expiryDate;
+     private String CVNum;
+    
+      private static CreditCard currentCreditCard;
+  
+     
+     public static CreditCard getCurrentCreditCard() {
+         return currentCreditCard;
+     }
+ 
+     public static void setCurrentCreditCard(CreditCard currentCreditCard) {
+         CreditCard.currentCreditCard = currentCreditCard;
+     }
+ 
+     public String getNameOnCard() {
+         return NameOnCard;
+     }
+ 
+     public void setNameOnCard(String NameOnCard) {
+         this.NameOnCard = NameOnCard;
+     }
+ 
+     public String getCreditCardNum() {
+         return creditCardNum;
+     }
+ 
+     public void setCreditCardNum(String creditCardNum) {
+         this.creditCardNum = creditCardNum;
+     }
+ 
+     public String getExpiryDate() {
+         return expiryDate;
+     }
+ 
+     public void setExpiryDate(String expiryDate) {
+         this.expiryDate = expiryDate;
+     }
+ 
+     public String getCVNum() {
+         return CVNum;
+     }
+ 
+     public void setCVNum(String CVNum) {
+         this.CVNum = CVNum;
+     }
+   
+  public static void setCreditCardInstance(CreditCard currentCreditCard){
+         CreditCard.currentCreditCard = currentCreditCard;
+     }
+     
+     public static CreditCard getCreditCardInstance(){
+         return currentCreditCard;
+     }
+ }
